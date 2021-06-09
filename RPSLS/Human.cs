adb1f5bc1eb72f9b523379e59a37acc9742dc87f
DAVID paredes <augustspies22@gmail.com>
2021-06-09 13:23:32 -0700

    Created Player and a Human Choice

---
+++ RPSLS/Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    public class Human
+    {
+        public static string choice()
+        {
+            Console.WriteLine("The choices are: ");
+            foreach (string index in Player.CurrentGesture())
+            {
+               Console.WriteLine(index);
+            }
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("What is your choice of gesture?");
+                string player_gesture = Console.ReadLine();
+                foreach (string item in Player.CurrentGesture()) { 
+                    if (item == player_gesture)
+                    {
+                        return player_gesture;
+                    }
+                }
+            }
+        }        
+    }
+}
---
+++ RPSLS/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    public class Player
+    {
+        
+        public static string name = " ";
+        public static string choice = " ";
+        
+        public static List<string> CurrentGesture()
+        {
+            List<string> chosen_gesture = new List<string>();
+            chosen_gesture.Add("Rock");
+            chosen_gesture.Add("Paper");
+            chosen_gesture.Add("Scissors");
+            chosen_gesture.Add("Lizard");
+            chosen_gesture.Add("Spock");
+            return chosen_gesture;
+        }
+       
+    }
+}
